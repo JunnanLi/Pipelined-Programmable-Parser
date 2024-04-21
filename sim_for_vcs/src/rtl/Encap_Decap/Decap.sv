81f6bb631d00618ca2630c2316d8c71733787407
lijunnan <nudt_ljn@163.com>
2024-04-21 11:27:11 +0800

    fix Decap func

--- sim_for_vcs/src/rtl/Encap_Decap/Decap.sv
+++ sim_for_vcs/src/rtl/Encap_Decap/Decap.sv
@@ -26,6 +26,7 @@ module Decap_Head(
   input   wire  [                       3:0]  i_metaSliceOffset,
   input   wire  [`HEAD_SHIFT_WIDTH-1:0]       i_metaDataOffset,
   input   wire  [`META_SHIFT_WIDTH-1:0]       i_decapLength,
+  input   wire                                i_decapEn
 );
 
   //====================================================================//
@@ -34,53 +35,56 @@ module Decap_Head(
   //* r_head/r_meta is used to output;
   //* r_preHead/r_preMeta is one clk delay of i_head/i_meta
   reg   [`HEAD_WIDTH+`TAG_WIDTH-1:0]    r_head, r_preHead;
-  reg   [`META_WIDTH+`TAG_WIDTH-1:0]    r_meta, r_preMeta, r_prePreMeta;
+  reg   [`META_WIDTH+`TAG_WIDTH-1:0]    r_meta, r_preMeta;
+  logic [`META_WIDTH-1:0]               l_meta;
   //* r_extMeta is part of i_metaShift
-  //* w_2head/w_2meta is used to shift
-  wire  [2*`HEAD_WIDTH-1:0]             w_2head;
+  //* w_2meta is used to shift
   wire  [2*`META_WIDTH-1:0]             w_2meta;
-  //* r_headShift/r_metaShift is record of i_headShift/w_metaShift
   wire                                  w_startBit_headTag, w_validBit_metaTag, w_validBit_headTag;
-  reg   [`META_SHIFT_WIDTH-1:0]         r_metaShift;
   reg   [                       3:0]    r_cnt_slice, r_metaSliceOffset;
   reg   [`META_SHIFT_WIDTH-1:0]         r_metaDataOffset;
   reg   [`META_SHIFT_WIDTH-1:0]         r_decapLength;
+  reg                                   r_decapEn;
   //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//  
   assign w_startBit_headTag = i_head[`HEAD_WIDTH+`TAG_START_BIT];
   assign w_validBit_headTag = r_preHead[`HEAD_WIDTH+`TAG_VALID_BIT];
   assign w_validBit_metaTag = r_preMeta[`META_WIDTH+`TAG_VALID_BIT];
-  assign w_2head    = {r_preHead[0+:`HEAD_WIDTH], i_head[0+:`HEAD_WIDTH]};
   assign w_2meta    = {r_preMeta[0+:`META_WIDTH], i_meta[0+:`META_WIDTH]};
   assign o_head     = r_head;
   assign o_meta     = r_meta;
 
+  //* shift meta;
+  always_comb begin
+    for(integer idx=0; idx<`META_CANDI_NUM; idx=idx+1)
+      if(r_decapLength == idx)
+        l_meta      = w_2meta[2*`META_WIDTH-idx*`SHIFT_WIDTH-1-:`META_WIDTH];
+  end
+
 	always_ff @(posedge i_clk) begin
-    {r_prePreMeta,r_preMeta}                    <= {r_preMeta,i_meta};
+    r_preMeta                                   <= i_meta;
     r_preHead                                   <= i_head;
     r_head                                      <= r_preHead;
-    r_meta                                      <= i_meta;
+    r_meta                                      <= r_preMeta;
     r_metaSliceOffset <= w_startBit_headTag? i_metaSliceOffset:r_metaSliceOffset;
     r_metaDataOffset  <= w_startBit_headTag? i_metaDataOffset: r_metaDataOffset;
     r_decapLength     <= w_startBit_headTag? i_decapLength:    r_decapLength;
-    r_metaShift       <= w_startBit_headTag? (i_decapLength + i_metaDataOffset): r_metaShift;
+    r_decapEn         <= w_startBit_headTag? i_decapEn:        r_decapEn;
     r_cnt_slice       <= i_meta[`META_WIDTH+`TAG_START_BIT]? 4'b0: 
-                          i_meta[`META_WIDTH+`TAG_VALID_BIT] (4'b1 + r_cnt_slice): 4'b0;
-    if(w_startBit_headTag == 1'b1 && i_metaSliceOffset == 4'b0) begin
-      //* decap first slice;
-      r_meta[`META_WIDTH+:META_SHIFT_WIDTH]     <= i_metaDataOffset;
-    end
-    else if(w_validBit_metaTag && r_cnt_slice == r_metaSliceOffset) begin
+                          i_meta[`META_WIDTH+`TAG_VALID_BIT]? (4'b1 + r_cnt_slice): 4'b0;
+    if(w_validBit_metaTag && r_cnt_slice == r_metaSliceOffset && r_decapEn == 1'b1) begin
       r_meta                                    <= r_preMeta;
       for(integer idx=0; idx<`META_CANDI_NUM; idx=idx+1) begin
-        if(r_metaShift == idx)
-          r_meta[0+:`META_WIDTH]                <= w_2meta[2*`META_WIDTH-idx*`SHIFT_WIDTH-1-:`META_WIDTH];
+        if(r_metaDataOffset <= idx)
+          r_meta[`META_WIDTH-1-idx*`SHIFT_WIDTH-:`SHIFT_WIDTH] <= l_meta[`META_WIDTH-1-idx*`SHIFT_WIDTH-:`SHIFT_WIDTH];
       end
-      r_meta[`META_WIDTH+:META_SHIFT_WIDTH]     <= ~r_metaShift;
-      r_meta[`TAG_START_BIT+`HEAD_WIDTH]        <= 1'b0;
-      r_meta[`META_WIDTH+`TAG_VALID_BIT]        <= |r_metaShift;
+      //* TODO, calculate valid_bit;
+      // r_meta[`META_WIDTH+:META_SHIFT_WIDTH]     <= ~r_metaShift;
+      // r_meta[`TAG_START_BIT+`HEAD_WIDTH]        <= 1'b0;
+      // r_meta[`META_WIDTH+`TAG_VALID_BIT]        <= |r_metaShift;
     end
-    else if(w_validBit_metaTag && r_cnt_slice > r_cnt_slice) begin
-      r_meta                                    <= r_preMeta;
+    else if(w_validBit_metaTag && r_cnt_slice > r_metaSliceOffset && r_decapEn == 1'b1) begin
+      r_meta[`META_WIDTH-1:0]                   <= l_meta;
+      r_meta[`TAG_START_BIT+`HEAD_WIDTH]        <= 1'b0;
     end
 	end
 
--- sim_for_vcs/verdiLog/verdi.cmd
+++ sim_for_vcs/verdiLog/verdi.cmd
@@ -3,38 +3,19 @@ debImport "-sverilog" "+v2k" "-f" "file_list.f" "-top" "Testbench_wrapper"
 debLoadSimResult \
            /home/lijunnan/Documents/0-code/vcs_prj/Programmable-Parser/wave.fsdb
 wvCreateWindow
-wvSetCursor -win $_nWave2 6855757.575758
+wvSetCursor -win $_nWave2 3621200.311769
 wvRestoreSignal -win $_nWave2 \
-           "/home/lijunnan/Documents/0-code/vcs_prj/Programmable-Parser/test_deparser.rc" \
+           "/home/lijunnan/Documents/0-code/vcs_prj/Programmable-Parser/test_decap.rc" \
            -overWriteAutoAlias on -appendSignals on
-wvScrollDown -win $_nWave2 0
-wvScrollDown -win $_nWave2 0
-wvScrollDown -win $_nWave2 0
-wvScrollDown -win $_nWave2 1
-wvScrollDown -win $_nWave2 1
-wvScrollDown -win $_nWave2 1
-wvScrollDown -win $_nWave2 1
-wvScrollUp -win $_nWave2 1
-wvScrollUp -win $_nWave2 1
-wvScrollUp -win $_nWave2 1
-wvScrollUp -win $_nWave2 1
+wvZoom -win $_nWave2 9320654.715511 12752922.837101
+wvSetCursor -win $_nWave2 10404106.460829 -snap {("G1" 6)}
+wvSetCursor -win $_nWave2 10631497.567871 -snap {("G1" 6)}
+debReload
 wvZoomOut -win $_nWave2
 wvZoomOut -win $_nWave2
-wvZoom -win $_nWave2 11247697.468954 14599511.314702
-wvSelectSignal -win $_nWave2 {( "G1" 4 )} 
-wvSetCursor -win $_nWave2 12536912.448146 -snap {("G1" 4)}
-wvSetMarker -win $_nWave2 -keepViewRange -name "1st_pkt" 12500000.000000 \
-           ID_GREEN5 long_dashed
-wvScrollDown -win $_nWave2 1
-wvSetCursor -win $_nWave2 13111509.107417 -snap {("G2" 1)}
-wvScrollDown -win $_nWave2 1
-wvSetCursor -win $_nWave2 13715125.799985 -snap {("G2" 5)}
-wvSetCursor -win $_nWave2 14327448.502541 -snap {("G2" 6)}
-wvSetCursor -win $_nWave2 13915364.029731 -snap {("G2" 6)}
-wvSetCursor -win $_nWave2 13729635.816633 -snap {("G2" 5)}
-wvSelectSignal -win $_nWave2 {( "G2" 2 )} 
-wvDisplayGridCount -win $_nWave2 -off
-wvGetSignalClose -win $_nWave2
-wvReloadFile -win $_nWave2
-wvSetCursor -win $_nWave2 14286820.455927 -snap {("G2" 6)}
-debExit
+wvZoomOut -win $_nWave2
+wvZoomOut -win $_nWave2
+wvZoom -win $_nWave2 18704924.946342 26152652.499348
+wvZoom -win $_nWave2 20074888.861000 22112419.937466
+wvSetCursor -win $_nWave2 20807002.521367 -snap {("G1" 6)}
+wvSetCursor -win $_nWave2 21002338.704068 -snap {("G1" 6)}
